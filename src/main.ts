6de5af5e1bf3324ec2c5066a0105b59faac33820
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-09-06 13:16:59 +0200

    check out how to tweak headers

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,38 @@
+const CAMO_HEADER_VIA = 'Camo Asset Proxy';
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request));
 });
 
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
 async function handleRequest(request: Request): Promise<Response> {
-  return new Response('Hello worker 2!', {
-    headers: { 'content-type': 'text/plain' },
+  if (request.method !== 'GET')
+    return new Response('Method not allowed', {
+      status: 405,
+      headers: {
+        Allow: 'GET',
+      },
+    });
+
+  return proxyImage(
+    'https://preview.redd.it/zin69hb06vl71.jpg?width=4624&format=pjpg&auto=webp&s=4c982611ef6f1c45806830fe4d736392cc8fc034'
+  );
+}
+
+async function proxyImage(url: string): Promise<Response> {
+  const proxied = await fetch(url, {
+    headers: {
+      Via: CAMO_HEADER_VIA,
+      'User-Agent': CAMO_HEADER_VIA,
+    },
   });
+
+  const contentType = proxied.headers.get('Content-Type');
+
+  const res = new Response(proxied.body, {
+    headers: {
+      ...(contentType ? { 'Content-Type': contentType } : {}),
+    },
+  });
+
+  return res;
 }
