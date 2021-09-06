781c09ba0c393477e64a2a1e817aa0bd64322ff6
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-09-06 12:29:48 +0200

    use prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+  event.respondWith(handleRequest(event.request));
+});
 /**
  * Respond with hello worker text
  * @param {Request} request
@@ -8,5 +8,5 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
   return new Response('Hello worker!', {
     headers: { 'content-type': 'text/plain' },
-  })
+  });
 }
