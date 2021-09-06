2eb3818a2a7c5da6d000aecc5883b4cf2debd983
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-09-06 13:22:03 +0200

    list acceptable mime types

---
+++ src/mime.ts
@@ -0,0 +1,11 @@
+export const acceptable = [
+  'image/bmp',
+  'image/gif',
+  'image/jp2',
+  'image/jpeg',
+  'image/jpg',
+  'image/png',
+  'image/svg+xml',
+  'image/tiff',
+  'image/webp',
+];
