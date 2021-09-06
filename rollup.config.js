286691ffeb256822c548fb1971953a977458bc36
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-09-06 17:46:26 +0200

    tweak .prettierignore

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ const typescript = require('@rollup/plugin-typescript');
 
 module.exports = {
   input: 'src/main.ts',
-  output: [{
-    file: 'dist/worker.js',
-    format: 'iife',
-  }],
+  output: [
+    {
+      file: 'dist/worker.js',
+      format: 'iife',
+    },
+  ],
   plugins: [typescript()],
 };
